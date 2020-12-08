9abff0a27d0d7113ee4581eb915f13d814348dba
Martha Cryan <martha.cryan@ibm.com>
2020-12-08 01:40:59 -0500

    ADd templating

--- workcarousel.js
+++ workcarousel.js
@@ -1,8 +1,18 @@
-const siema = new Siema();
 
-$('.prev').click(function(event) {
-  siema.prev();
-});
-$('.next').click(function(event) {
-  siema.next();
+[].forEach.call(document.getElementsByClassName("siema"), (value, index) => {
+  const siema = new Siema({
+    selector: '#'+value.id
+  });
+
+  const total = $('#'+value.id).children().first().children().length;
+  $('#'+value.parentElement.parentElement.id+' p').text((siema.currentSlide + 1) + ' / '+ total);
+
+  $('#'+value.parentElement.parentElement.id+' .prev').click(function(event) {
+    siema.prev();
+    $('#'+value.parentElement.parentElement.id+' p').text((siema.currentSlide + 1) + ' / '+ total);
+  });
+  $('#'+value.parentElement.parentElement.id+' .next').click(function(event) {
+    siema.next();
+    $('#'+value.parentElement.parentElement.id+' p').text((siema.currentSlide + 1) + ' / '+ total);
+  });
 });
