fca1d559b11731a2a4a7118b748d6cbbf6ea4848
Martha Cryan <martha.cryan@ibm.com>
2020-12-12 22:33:30 -0600

    sold out

--- getsoldout.js
+++ getsoldout.js
@@ -1,10 +1,21 @@
-const Http = new XMLHttpRequest();
-const url='https://app.snipcart.com/api/products';
-Http.open("GET", url);
-Http.setRequestHeader('Accept', 'application/json');
-Http.setRequestHeader('X-Snipcart-RequestToken', 'MzA3NzI3NDAtYzc2NC00YWZiLWE3NTYtZjk5NTcwMDIxN2NmNjM3NDI5NTYzNjQ1MTA4Mjkw');
-Http.send();
+// const Http = new XMLHttpRequest();
+// const url='https://app.snipcart.com/api/products';
+// Http.open("GET", url);
+// Http.setRequestHeader('Accept', 'application/json');
+// Http.setRequestHeader('X-Snipcart-RequestToken', 'MzA3NzI3NDAtYzc2NC00YWZiLWE3NTYtZjk5NTcwMDIxN2NmNjM3NDI5NTYzNjQ1MTA4Mjkw');
+// Http.send();
 
-Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
+const start = async function() {
+	const secret = "MzA3NzI3NDAtYzc2NC00YWZiLWE3NTYtZjk5NTcwMDIxN2NmNjM3NDI5NTYzNjQ1MTA4Mjkw"
+
+	const request = await fetch('https://app.snipcart.com/api/products', {
+	    headers: {
+	        'Authorization': `Basic ${btoa(secret)}`,
+	        'Accept': 'application/json'
+	    }
+	})
+
+	const result = await request.json()
+	console.log(result)
 }
+start()
