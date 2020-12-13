29b1bfeb1a96df6d5480bb695870c5d2987ac12a
Martha Cryan <martha.cryan@ibm.com>
2020-12-12 21:40:52 -0600

    sold

--- getsoldout.js
+++ getsoldout.js
@@ -2,7 +2,7 @@ const Http = new XMLHttpRequest();
 const url='https://app.snipcart.com/api/products/AP1';
 Http.open("GET", url);
 Http.setRequestHeader('Accept', 'application/json');
-Http.setRequestHeader('Authorization', 'Basic MzA3NzI3NDAtYzc2NC00YWZiLWE3NTYtZjk5NTcwMDIxN2NmNjM3NDI5NTYzNjQ1MTA4Mjkw');
+Http.setRequestHeader('X-Snipcart-RequestToken', 'MzA3NzI3NDAtYzc2NC00YWZiLWE3NTYtZjk5NTcwMDIxN2NmNjM3NDI5NTYzNjQ1MTA4Mjkw');
 Http.send();
 
 Http.onreadystatechange = (e) => {
